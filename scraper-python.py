9057839eda53784957dec51692b068ea2592e12c
Lucas <lucasbgirardi@gmail.com>
2026-04-17 23:46:39 -0300

    feat: codigo testado com header e funcionando

--- scraper-python.py
+++ scraper-python.py
@@ -1,14 +1,22 @@
 import requests
 
-# Comando abaixo serve para setar a URL do site
-url_amazon = "https://www.amazon.com.br/s?k=rtx+5050&i=computers&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=1DXZWAH23YIV3&sprefix=rtx+505%2Ccomputers%2C200&ref=nb_sb_noss_2"
+# Comando de ativação do venv : .\venv\Scripts\Activate.ps1
 
-# Comando abaixo serve para fazer requisições GET
-response = requests.get(url_amazon)
+# Criando a mascara de acesso:
+headers = {
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
+}
+
+# Comando abaixo serve para setar a URL do site
+url_test = "http://books.toscrape.com/"
+
+# Comando abaixo serve para fazer requisições GET (adicionei o header para realizar o get)
+response = requests.get(url_test, headers=headers)
 
 # Abaixo irei verificar o status e acessar os dados (se conseguir acessar)
 if response.status_code == 200:
-    dados = response.text()   # Este comando converte a resposta da requisição para Python
+    dados = response.text   # Este comando converte a resposta da requisição para Python
     print(dados)
+    print(len(dados))
 else:
     print(f"Erro: {response.status_code}")
